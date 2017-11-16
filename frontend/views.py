d88ad704f24472ee749c511146b024a1f4c86828
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2017-11-16 23:49:23 +0100

    added input fields its implimented by a ModelForm generated from SlaveModel

--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, View
+from django.views.generic.edit import SingleObjectMixin, FormMixin
+from django.core.urlresolvers import reverse
+
+
+from django.http import HttpResponseForbidden
 
 from .models import Slave as SlaveModel
+
+from django.forms import modelform_factory
 
 
 class WelcomeView(TemplateView):
     template_name = 'frontend/welcome.html'
 
-
-class SlavesView(ListView):
+class SlavesViewLul(ListView):
     template_name = 'frontend/slaves.html'
     model = SlaveModel
     context_object_name = 'slaves'
+    #form = modelform_factory(SlaveModel,fields='__all__')
+
+
+class SlavesView(FormMixin, ListView):
+    template_name = "frontend/slaves.html"
+    model = SlaveModel
+    context_object_name = "slaves"
+    form_class = modelform_factory(SlaveModel,fields='__all__')
