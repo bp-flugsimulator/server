af2d5528d9ebe97bdb89fd5b3c99eec0c258d353
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2017-11-17 09:08:26 +0100

    added api call to add a Slave

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
 from frontend import views
+from frontend import api
 
 app_name = 'frontend'
 
 urlpatterns = [
     url(r'^welcome', views.WelcomeView.as_view(), name='welcome'),
     url(r'^slaves/$', views.SlavesView.as_view(), name='slaves'),
+    url(r'^api/addSlave', api.addSlave, name='addSlaves')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,28 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, FormView, View
-from django.views.generic.edit import SingleObjectMixin, FormMixin
-from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import FormMixin
 
-
-from django.http import HttpResponseForbidden
 
 from .models import Slave as SlaveModel
-
-from django.forms import modelform_factory
-
+from .forms import SlaveForm
 
 class WelcomeView(TemplateView):
     template_name = 'frontend/welcome.html'
-
-class SlavesViewLul(ListView):
-    template_name = 'frontend/slaves.html'
-    model = SlaveModel
-    context_object_name = 'slaves'
-    #form = modelform_factory(SlaveModel,fields='__all__')
-
 
 class SlavesView(FormMixin, ListView):
     template_name = "frontend/slaves.html"
     model = SlaveModel
     context_object_name = "slaves"
-    form_class = modelform_factory(SlaveModel,fields='__all__')
+    form_class = SlaveForm
