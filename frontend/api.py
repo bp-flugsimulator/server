3a9728aaa21f2aa3295c1d8207ac3f98a741bd47
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2017-11-17 10:38:47 +0100

    added errorhandling by returning the error as a message in the cookie

--- frontend/api.py
+++ frontend/api.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponseForbidden, HttpResponseRedirect
 from .models import Slave as SlaveModel
 from .forms import SlaveForm
+from django.db.utils import DatabaseError
+
+from django.contrib import messages
+
 
 def addSlave(request):
     if request.method == 'POST':
@@ -9,7 +13,18 @@ def addSlave(request):
             name = form.cleaned_data['name']
             ip = form.cleaned_data['ip_address']
             mac = form.cleaned_data['mac_address']
-            SlaveModel(name=name, ip_address=ip,mac_address=mac).save()
+            try:
+                SlaveModel(name=name, ip_address=ip,mac_address=mac).save()
+            except DatabaseError as err:
+                messages.error(request, err)
+        else:
+            errormsg = ''
+            for value in form.errors.as_data().values():
+                errormsg += str(value[0])\
+                    .replace('\'','')\
+                    .replace('[','')\
+                    .replace(']','') + ' '
+            messages.error(request, errormsg)
         return HttpResponseRedirect("/slaves/")
     else:
         return HttpResponseForbidden
