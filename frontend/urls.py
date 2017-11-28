a99aaf2d83f72b4ed4066d70d4b40351c7e4b684
Heiko Carrasco <heiko.carrasco@yahoo.com>
2017-11-28 01:10:27 +0100

    Fixed order of urls so wol action comes first

--- frontend/urls.py
+++ frontend/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^welcome$', views.WelcomeView.as_view(), name='welcome'),
     url(r'^slaves/$', views.SlavesView.as_view(), name='slaves'),
     url(r'^api/slaves', api.add_slave, name='add_slaves'),
-    url(r'^api/slave/([0-9]+)', api.manage_slave, name='manage_slave'),
     url(r'^api/slave/([0-9]+)/wol/$', api.wol_slave, name='wol_slave'),
+    url(r'^api/slave/([0-9]+)', api.manage_slave, name='manage_slave'),
 ]
