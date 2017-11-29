ec06dbf7e2709e0365b547b1092ce7827acd98a4
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2017-11-29 16:56:30 +0100

    added ProgramForm

--- frontend/forms.py
+++ frontend/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
 from .models import Slave as SlaveModel
+from .models import Program as ProgramModel
 
 class SlaveForm(ModelForm):
     class Meta:
         model = SlaveModel
         fields = '__all__'
+
+class ProgramForm(ModelForm):
+    class Meta:
+        model = SlaveModel
+        fields = ['name','command']
