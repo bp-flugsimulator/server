96e02f8b9243082b6cca045bf186232575fa9754
Heiko Carrasco <heiko.carrasco@yahoo.com>
2017-11-30 14:34:32 +0100

    Removed consumer from testing

--- frontend/consumers.py
+++ frontend/consumers.py
@@ -1,18 +1,18 @@
 from channels import Group
 
 # Connected to websocket.connect
-def ws_add(message):
+def ws_add(message): #pragma: no cover
     # Accept the connection
     message.reply_channel.send({"accept": True})
     # Add to the chat group
     Group('notifications').add(message.reply_channel)
 
 # Connected to websocket.receive
-def ws_message(message):
+def ws_message(message): #pragma: no cover
     Group('notifications').send({
         "text": "WIP TODO",
     })
 
 # Connected to websocket.disconnect
-def ws_disconnect(message):
+def ws_disconnect(message): #pragma: no cover
     Group('notifications').discard(message.reply_channel)
