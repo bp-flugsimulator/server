90974a7ee6d9f5abab2e7f48e60f41ecebad925c
Heiko Carrasco <heiko.carrasco@yahoo.com>
2017-12-23 19:36:26 +0100

    Fixed wrong sass and css path

--- server/management/commands/compilesass.py
+++ server/management/commands/compilesass.py
@@ -2,9 +2,11 @@ from django.core.management.base import BaseCommand
 
 import sass
 
+from server.settings import BASE_DIR
+
 class Command(BaseCommand):
     help = 'Compiles the custom sass to css'
 
     def handle(self, *args, **options):
-        sass.compile(dirname=('base/static/scss', 'base/static/css'))
+        sass.compile(dirname=('base/static/base/scss', 'base/static/base/css'))
         self.stdout.write('Compiled sass')
