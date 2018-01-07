78460606353db97c067ba1c6d50ef1a7931d10d7
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-01-07 17:04:01 +0100

    added command

---
+++ server/management/commands/clean_npm.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+import os
+
+class Command(BaseCommand):
+    help = 'Removes unused files from node_modules'
+
+    def handle(self, *args, **options):
+        html_files = []
+
+        for dirpath, dirnames, filenames in os.walk("."):
+            for filename in [f for f in filenames if f.endswith(".html")]:
+                filepath = os.path.join(dirpath, filename)
+                if 'node_modules' not in filepath:
+                    html_files.append(filepath)
+
+        dependencies = []
+        for path in html_files:
+            with open(path, 'r') as file:
+                for line in file:
+                    if '{% static' in line:
+                        line = line.split("'")[1]
+                        if 'node/' in line:
+                            line = line.replace('node/','./node_modules/')
+                            dependencies.append(line)
+
+        print(dependencies)
+
+        for dirpath, dirnames, filenames in os.walk("./node_modules"):
+            for filename in filenames:
+                filepath = dirpath + '/' + filename
+                if (filepath) not in dependencies:
+                    os.remove(filepath)
+
+
+        deleted = True
+        while deleted:
+            deleted = False
+            for dirpath, dirnames, filenames in os.walk("./node_modules"):
+                if not dirnames and not filenames:
+                    os.rmdir(dirpath)
+                    deleted = True
