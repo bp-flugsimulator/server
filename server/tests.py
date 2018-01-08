3d11d8c09e47e02de79865985e0d542102accf6e
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-01-08 11:02:31 +0100

    added test

--- server/tests.py
+++ server/tests.py
@@ -2,11 +2,12 @@ from django.test import TestCase
 
 from .utils import StatusResponse
 from utils.status import Status
-from server.management.commands.compilesass import Command
 from sass import CompileError
+from server.management.commands.compilesass import Command as SassCommand
+from server.management.commands.clean_npm import Command as CleanNpmCommand
 
 import json
-from os import remove
+from os import remove, replace, mkdir, rmdir
 from os.path import isfile, isdir
 
 
@@ -26,15 +27,81 @@ class StatusResponseTest(TestCase):
 
 class ManagementTest(TestCase):
     CSS_PATH = 'base/static/base/css/custom.css'
+    NPM_PATH = 'node_modules'
+    BACKUP_PATH = 'node_modules_backup'
 
     def test_css_generation(self):
         # if there is already a custom.css file remove it
         if isfile(self.CSS_PATH):
             remove(self.CSS_PATH)
-        com = Command()
+        com = SassCommand()
         if not isdir('node_modules'):
             with self.assertRaises(CompileError):
                 com.handle()
         else:
             com.handle()
             self.assertTrue(isfile(self.CSS_PATH))
+        
+
+    def test_clean_npm(self):
+        # if node_modules exists back it up
+        if isdir(self.NPM_PATH):
+            replace(src=self.NPM_PATH, dst=self.BACKUP_PATH)
+
+        # create package directory
+        mkdir(self.NPM_PATH)
+
+        # create two packages
+        mkdir(self.NPM_PATH + '/test1')
+        with open(self.NPM_PATH + '/test1/' + 'testfile.txt', 'a') as f:
+            f.close()
+
+        mkdir(self.NPM_PATH + '/test1/testdir')
+        with open(self.NPM_PATH + '/test1/testdir/' + 'testfile.txt', 'a') as f:
+            f.close()
+
+        mkdir(self.NPM_PATH + '/test2')
+        with open(self.NPM_PATH + '/test2/' + 'testfile.txt', 'a') as f:
+            f.close()
+
+        # create test dependency
+        with open('test.html', 'w') as f:
+            f.write("{% static 'node/test1/testdir/testfile.txt' %}")
+            f.close()
+
+        # execute command
+        exception = None
+        try:
+            com = CleanNpmCommand()
+            com.handle()
+
+            self.assertTrue(isdir(self.NPM_PATH + '/test1/'))
+            self.assertTrue(isdir(self.NPM_PATH + '/test1/testdir'))
+            self.assertTrue(isfile(self.NPM_PATH + '/test1/testfile.txt'))
+            self.assertTrue(isfile(self.NPM_PATH + '/test1/testdir/testfile.txt'))
+
+            self.assertFalse(isdir(self.NPM_PATH + '/test2'))
+        except Exception as err:
+            exception = err
+            try:
+                remove(self.NPM_PATH + '/test2/' + 'testfile.txt')
+            except:
+                pass
+            try:
+                rmdir(self.NPM_PATH + '/test2')
+            except:
+                pass
+        finally:
+            # delete testfiles
+            remove('test.html')
+            remove(self.NPM_PATH + '/test1/testdir/' + 'testfile.txt')
+            remove(self.NPM_PATH + '/test1/' + 'testfile.txt')
+            rmdir(self.NPM_PATH + '/test1/testdir')
+            rmdir(self.NPM_PATH + '/test1')
+            rmdir(self.NPM_PATH)
+
+            # restore backup
+            if isdir(self.BACKUP_PATH):
+                replace(src=self.BACKUP_PATH, dst=self.NPM_PATH)
+
+        self.assertIsNone(exception)
