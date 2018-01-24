2e6070230720196749a89a1c45c7f42bf5a7161f
jonas <jonas.meurer@outlook.de>
2018-01-24 19:10:33 +0100

    init factory_boy

---
+++ frontend/tests/factory.py
@@ -0,0 +1,87 @@
+from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyText, FuzzyInteger
+from factory import SubFactory, Sequence
+from uuid import uuid4
+
+import socket, struct
+
+from frontend.models import (
+    Slave as SlaveModel,
+    SlaveStatus as SlaveStatusModel,
+    ProgramStatus as ProgramStatusModel,
+    Program as ProgramModel,
+    Script as ScriptModel,
+    ScriptGraphPrograms as SGP,
+    ScriptGraphFiles as SGF,
+    File as FileModel,
+)
+
+
+class SlaveFactory(DjangoModelFactory):
+    class Meta:
+        model = SlaveModel
+
+    name = FuzzyText(length=20, prefix="slave_")
+    ip_address = Sequence(lambda n: socket.inet_ntoa(struct.pack('!L', n)))
+    mac_address = Sequence(lambda n: '-'.join(a+b for a,b in zip("{:012x}".format(n)[::2], "{:012x}".format(n)[1::2])))
+
+
+class SlaveStatusFactory(DjangoModelFactory):
+    class Meta:
+        model = SlaveStatusModel
+
+    slave = SubFactory(SlaveFactory)
+    command_uuid = uuid4().hex
+    online = False
+
+
+class ProgramFactory(DjangoModelFactory):
+    class Meta:
+        model = ProgramModel
+
+    name = FuzzyText(length=20, prefix="program_")
+    path = FuzzyText(length=40)
+    arguments = FuzzyText(length=200)
+    slave = SubFactory(SlaveFactory)
+    start_time = FuzzyInteger(0)
+
+
+class ProgramStatusFactory(DjangoModelFactory):
+    class Meta:
+        model = ProgramStatusModel
+
+    program = SubFactory(ProgramFactory)
+    command_uuid = uuid4().hex
+    running = False
+    code = ""
+
+
+class FileFactory(DjangoModelFactory):
+    class Meta:
+        model = FileModel
+
+    name = FuzzyText(length=20, prefix="file_")
+    sourcePath = FuzzyText(length=100)
+    destinationPath = FuzzyText(length=100)
+    slave = SubFactory(SlaveFactory)
+
+
+class ScriptFactory(DjangoModelFactory):
+    class Meta:
+        model = ScriptModel
+
+    name = FuzzyText(length=20, prefix="script_")
+
+
+class SGPFactory(DjangoModelFactory):
+    class Meta:
+        model = SGP
+
+    index = FuzzyInteger(0)
+
+
+class SGFFactory(DjangoModelFactory):
+    class Meta:
+        model = SGF
+
+    index = FuzzyInteger(0)
--- frontend/tests/test_database.py
+++ frontend/tests/test_database.py
@@ -7,6 +7,15 @@ from datetime import datetime
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 
+from frontend.apps import flush
+
+from .factory import (
+    SlaveFactory,
+    SlaveStatusFactory,
+    ProgramFactory,
+    ProgramStatusFactory,
+)
+
 from frontend.models import (
     Slave as SlaveModel,
     SlaveStatus as SlaveStatusModel,
@@ -16,7 +25,8 @@ from frontend.models import (
     validate_argument_list,
 )
 
-class DatabaseTests(TestCase): # pylint: disable=unused-variable
+
+class DatabaseTests(TestCase):  # pylint: disable=unused-variable
     def test_validate_argument_list(self):
         self.assertIsNone(validate_argument_list('a b c d e f g'))
 
@@ -26,19 +36,10 @@ class DatabaseTests(TestCase): # pylint: disable=unused-variable
             'Enter a valid argument list.',
             validate_argument_list,
             'a "abc',
-)
-
-    def test_slave_has_err(self):
-        slave = SlaveModel(
-            name="test_slave",
-            ip_address="192.168.5.0",
-            mac_address="00:02:00:00:00:00",
         )
 
-        slave.save()
-
-        prog = ProgramModel(slave=slave, name="test_prog", path="None")
-        prog.save()
+    def test_slave_has_err(self):
+        slave = SlaveFactory()
 
         self.assertFalse(slave.has_error)
         self.assertFalse(slave.has_running)
@@ -55,49 +56,17 @@ class DatabaseTests(TestCase): # pylint: disable=unused-variable
         self.assertFalse(slave.is_online)
 
     def test_program_is_err(self):
-        slave = SlaveModel(
-            name="test_slave",
-            ip_address="192.168.5.0",
-            mac_address="00:02:00:00:00:00",
-        )
-
-        slave.save()
-
-        prog = ProgramModel(slave=slave, name="test_prog", path="None")
-        prog.save()
+        prog = ProgramFactory()
 
         self.assertFalse(prog.is_running)
         self.assertFalse(prog.is_error)
         self.assertFalse(prog.is_successful)
         self.assertFalse(prog.is_executed)
 
-    def test_slave_insert_valid(self):
-        mod = SlaveModel(
-            name="Tommo3",
-            ip_address="192.168.5.39",
-            mac_address="00:02:00:00:00:00",
-        )
-        mod.full_clean()
-        mod.save()
-        self.assertTrue(SlaveModel.objects.filter(name="Tommo3").exists())
-
     def test_program_running(self):
-        slave = SlaveModel(
-            name="test_slave",
-            ip_address="0.0.2.0",
-            mac_address="00:00:00:00:00:00",
-        )
-        slave.save()
-
-        prog = ProgramModel(name="test_program", path="none", slave=slave)
-        prog.save()
-
-        ProgramStatusModel(
-            command_uuid=uuid4(),
-            code="",
-            program=prog,
-            running=True,
-        ).save()
+        status = ProgramStatusFactory(running=True)
+        prog = status.program
+        slave = prog.slave
 
         self.assertTrue(slave.has_running)
         self.assertFalse(slave.has_error)
@@ -107,22 +76,9 @@ class DatabaseTests(TestCase): # pylint: disable=unused-variable
         self.assertFalse(prog.is_error)
 
     def test_program_successful(self):
-        slave = SlaveModel(
-            name="test_slave",
-            ip_address="0.0.2.0",
-            mac_address="00:00:00:00:00:00",
-        )
-        slave.save()
-
-        prog = ProgramModel(name="test_program", path="none", slave=slave)
-        prog.save()
-
-        ProgramStatusModel(
-            command_uuid=uuid4(),
-            code="0",
-            program=prog,
-            running=False,
-        ).save()
+        status = ProgramStatusFactory(code="0")
+        prog = status.program
+        slave = prog.slave
 
         self.assertFalse(slave.has_running)
         self.assertFalse(slave.has_error)
@@ -132,22 +88,9 @@ class DatabaseTests(TestCase): # pylint: disable=unused-variable
         self.assertFalse(prog.is_error)
 
     def test_program_error(self):
-        slave = SlaveModel(
-            name="test_slave",
-            ip_address="0.0.2.0",
-            mac_address="00:00:00:00:00:00",
-        )
-        slave.save()
-
-        prog = ProgramModel(name="test_program", path="none", slave=slave)
-        prog.save()
-
-        ProgramStatusModel(
-            command_uuid=uuid4(),
-            code="1",
-            program=prog,
-            running=False,
-        ).save()
+        status = ProgramStatusFactory(code="1")
+        prog = status.program
+        slave = prog.slave
 
         self.assertFalse(slave.has_running)
         self.assertTrue(slave.has_error)
@@ -157,16 +100,12 @@ class DatabaseTests(TestCase): # pylint: disable=unused-variable
         self.assertTrue(prog.is_error)
 
     def test_flush_error(self):
-        from frontend.apps import flush
-        SlaveModel(
-            name='test_flush_error',
-            ip_address='0.1.0.0',
-            mac_address='00:01:00:00:00:00',
-        ).save()
+        slave = SlaveFactory()
+
         flush('Slave')
         flush('UnknownModel')
-        self.assertFalse(
-            SlaveModel.objects.filter(name='test_flush_error').exists())
+
+        self.assertFalse(SlaveModel.objects.filter(name=slave.name).exists())
 
     def test_slave_insert_invalid_ip(self):
         self.assertRaises(
@@ -217,31 +156,15 @@ class DatabaseTests(TestCase): # pylint: disable=unused-variable
         )
 
     def test_flush(self):
-        slave = SlaveModel(
-            name="test",
-            ip_address="127.0.0.1",
-            mac_address="00:00:00:00:00:00",
-        )
-        slave.save()
+        slave = SlaveFactory()
+        prog = ProgramFactory(slave=slave)
 
-        prog = ProgramModel(
-            name="test",
-            path="None",
-            arguments="None",
-            slave=slave,
-        )
-        prog.save()
-
-        status_slave = SlaveStatusModel(slave=slave, command_uuid='abc')
-        status_slave.save()
-
-        status_program = ProgramStatusModel(prog.id, "None", datetime.now())
-        status_program.save()
+        status_slave = SlaveStatusFactory(slave=slave)
+        status_program = ProgramStatusFactory(program=prog)
 
         self.assertEqual(SlaveStatusModel.objects.count(), 1)
         self.assertEqual(ProgramStatusModel.objects.count(), 1)
 
-        from frontend.apps import flush
         flush("SlaveStatus", "ProgramStatus")
 
         self.assertEqual(SlaveStatusModel.objects.count(), 0)
