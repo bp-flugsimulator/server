ee38ee162f7ed081f981038c599ba98f78c57d3a
Heiko Carrasco <Miterion@users.noreply.github.com>
2018-01-28 13:30:20 +0100

    Split tests into multiple files (#79)
    * Split tests
    
    * removed broken merge
    
    * Added download tests
    
    * Added missing tests
    
    * Fixed codacy errors
    
    * Fixed styling issues
    
    * More style fixes
    
    * Removed unused imports
    
    * Removed deletes of models at the end of tests
    
    * init factory_boy
    
    * changed requirements
    
    * first test file changed
    
    * clean up
    
    * cleaned download tests
    
    * fixed frontend tests
    
    * changed script tests
    
    * fixed websockets tests
    
    * added factory_boy wheel
    
    * added faker wheel
    
    * added faker depdencies
    
    * fixed code style
    
    * removed manual slave creation
    replaced test_script with factory
    
    * clean

---
+++ frontend/tests/factory.py
@@ -0,0 +1,108 @@
+"""
+Database factories for tests
+"""
+import socket
+import struct
+from uuid import uuid4
+from factory import SubFactory, Sequence
+from factory.django import DjangoModelFactory
+from factory.fuzzy import FuzzyText, FuzzyInteger
+
+from frontend.models import (
+    Slave as SlaveModel,
+    SlaveStatus as SlaveStatusModel,
+    ProgramStatus as ProgramStatusModel,
+    Program as ProgramModel,
+    Script as ScriptModel,
+    ScriptGraphPrograms as SGP,
+    ScriptGraphFiles as SGF,
+    File as FileModel,
+)
+
+
+def int_to_mac(integer):
+    """
+    Converts a an integer into a mac address, with `:` notation.
+
+    Arguments
+    ---------
+        integer: int
+
+    Returns
+    -------
+        str
+    """
+    first = "{:012x}".format(integer)[::2]
+    second = "{:012x}".format(integer)[1::2]
+
+    return ':'.join(a + b for a, b in zip(first, second))
+
+
+class SlaveFactory(DjangoModelFactory):
+    class Meta:
+        model = SlaveModel
+
+    name = FuzzyText(length=20, prefix="slave_")
+    ip_address = Sequence(lambda n: socket.inet_ntoa(struct.pack('!L', n)))
+    mac_address = Sequence(int_to_mac)
+
+
+class SlaveStatusFactory(DjangoModelFactory):
+    class Meta:
+        model = SlaveStatusModel
+
+    slave = SubFactory(SlaveFactory)
+    command_uuid = uuid4().hex
+    online = False
+
+
+class ProgramFactory(DjangoModelFactory):
+    class Meta:
+        model = ProgramModel
+
+    name = FuzzyText(length=20, prefix="program_")
+    path = FuzzyText(length=40)
+    arguments = FuzzyText(length=200)
+    slave = SubFactory(SlaveFactory)
+    start_time = FuzzyInteger(0)
+
+
+class ProgramStatusFactory(DjangoModelFactory):
+    class Meta:
+        model = ProgramStatusModel
+
+    program = SubFactory(ProgramFactory)
+    command_uuid = uuid4().hex
+    running = False
+    code = ""
+
+
+class FileFactory(DjangoModelFactory):
+    class Meta:
+        model = FileModel
+
+    name = FuzzyText(length=20, prefix="file_")
+    sourcePath = FuzzyText(length=100)
+    destinationPath = FuzzyText(length=100)
+    slave = SubFactory(SlaveFactory)
+
+
+class ScriptFactory(DjangoModelFactory):
+    class Meta:
+        model = ScriptModel
+
+    name = FuzzyText(length=20, prefix="script_")
+
+
+class SGPFactory(DjangoModelFactory):
+    class Meta:
+        model = SGP
+
+    index = FuzzyInteger(0)
+
+
+class SGFFactory(DjangoModelFactory):
+    class Meta:
+        model = SGF
+
+    index = FuzzyInteger(0)
---
+++ frontend/tests/test_download.py
@@ -0,0 +1,110 @@
+#  pylint: disable=C0111,C0103
+
+from os import getcwd, remove, mkdir, rmdir
+from os.path import join, isdir
+
+from django.test import TestCase
+from django.urls import reverse
+
+
+class DownloadTests(TestCase):
+    DOWNLOAD_FOLDER = 'downloads'
+
+    @classmethod
+    def setUpClass(cls):
+        if not isdir(cls.DOWNLOAD_FOLDER):
+            mkdir(cls.DOWNLOAD_FOLDER)
+        super().setUpClass()
+
+    def test_download_page_no_folder(self):
+        rmdir(self.DOWNLOAD_FOLDER)
+        response = self.client.get(reverse('frontend:downloads'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            'No files are present in the download folder',
+            str(response.content),
+        )
+
+    def test_download_page_0_byte(self):
+        with open(
+                join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile1.txt'),
+                'w',
+        ) as file:
+            file.close()
+
+        response = self.client.get(reverse('frontend:downloads'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '0 B',
+            str(response.content),
+        )
+        self.assertIn(
+            'href="/static/downloads/testfile1.txt"',
+            str(response.content),
+        )
+
+        remove(join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile1.txt'), )
+
+    def test_download_page_1_kib(self):
+        with open(
+                join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile2.txt'),
+                'w',
+        ) as file:
+            file.seek(pow(2, 10))
+            file.write('\0')
+            file.close()
+
+        response = self.client.get(reverse('frontend:downloads'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '1 KiB',
+            str(response.content),
+        )
+        self.assertIn(
+            'href="/static/downloads/testfile2.txt"',
+            str(response.content),
+        )
+
+        remove(join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile2.txt'), )
+
+    def test_download_page_1_mib(self):
+        with open(
+                join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile3.txt'),
+                'w',
+        ) as file:
+            file.seek(pow(2, 20))
+            file.write('\0')
+            file.close()
+
+        response = self.client.get(reverse('frontend:downloads'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '1 MiB',
+            str(response.content),
+        )
+        self.assertIn(
+            'href="/static/downloads/testfile3.txt"',
+            str(response.content),
+        )
+
+        remove(join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile3.txt'), )
+
+    def test_download_page_1_gib(self):
+        with open(
+                join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile4.txt'),
+                'w',
+        ) as file:
+            file.seek(pow(2, 30))
+            file.write("\0")
+            file.close()
+
+        response = self.client.get(reverse('frontend:downloads'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '1 GiB',
+            str(response.content),
+        )
+        self.assertIn('href="/static/downloads/testfile4.txt"',
+                      str(response.content))
+
+        remove(join(getcwd(), self.DOWNLOAD_FOLDER, 'testfile4.txt'), )
