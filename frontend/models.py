f5c5da265da49e7d0919b46a97e34603fb80a0b5
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-03-26 00:14:24 +0200

    fixed migrations

--- frontend/models.py
+++ frontend/models.py
@@ -858,4 +858,4 @@ class ProgramStatus(Model):
     command_uuid = CharField(max_length=32, unique=True)
     running = BooleanField(unique=False, default=True)
     timeouted = BooleanField(unique=False, default=False)
-    start_time = DateTimeField(default=0)
+    start_time = DateTimeField()
